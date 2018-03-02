3560ff1bfc21ed2c3aa51b804f20a15db1190dfd
Ben Petallana <cooldirtyboy@gmail.com>
2018-03-02 11:54:21 +0800

    Added FileLocked checking

--- JerkyTool/JerkyTool/Extension.cs
+++ JerkyTool/JerkyTool/Extension.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -62,6 +63,33 @@ namespace JerkyTool
         public static T ToEnumOnly<T>(this string enumString)
         {
             return (T)Enum.Parse(typeof(T), enumString);
+        }
+
+
+        public static bool IsFileLocked(this FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.Read, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
         }
     }
 }
