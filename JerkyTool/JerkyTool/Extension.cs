4d4d860770e2a89e2a7269b4de58d3ff1e5fb259
Ben Petallana <sydpol@yahoo.com>
2018-03-05 11:51:41 +0800

    Revert "Added Numeric validation"
    This reverts commit 8c8ad15716fb16e51baebc831ce489b5f2e2af13.

--- JerkyTool/JerkyTool/Extension.cs
+++ JerkyTool/JerkyTool/Extension.cs
@@ -98,50 +98,5 @@ namespace JerkyTool
         {
             return Helper.Decrypt(s);
         }
-
-
-        public static bool IsNumeric(this object value)
-        {
-            if (value == null || value is DateTime)
-            {
-                return false;
-            }
-
-            if (value is Int16 || value is Int32 || value is Int64 || value is Decimal || value is Single || value is Double || value is Boolean)
-            {
-                return true;
-            }
-
-            try
-            {
-                if (value is string)
-                    Double.Parse(value as string);
-                else
-                    Double.Parse(value.ToString());
-                return true;
-            }
-            catch
-            {
-                return false;
-            }
-
-        }
-
-
-        public static bool IsNumeric(this string value)
-        {
-            if (value == null)
-            {
-                return false;
-            }
-
-            try
-            {
-                var num = Double.Parse(value);
-                return true;
-            }
-            catch { return false; }
-
-        }
     }
 }
